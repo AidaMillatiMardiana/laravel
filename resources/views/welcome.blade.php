0907ed5d6d7300f1c7b2759f79c875b21ae4f4cd
AidaMillatiMardiana <aidadiana2702@gmail.com>
2022-02-14 10:38:40 +0700

    Changes Welcome Blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,16 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
+            <div class="ml-4 text-lg leading-7 font-semibold">
+                <h1 class="text-grey-900 dark:text-white">
+                    Aida Millati Mardiana <br><br>
+                    Kelas: 2H, NIM: 2041720071
+                </h1></div>
+            </div>
+        </div>
+    </body>
+</html>
+            <!-- @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
@@ -34,7 +43,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif -->
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
