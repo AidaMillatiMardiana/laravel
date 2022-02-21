c17942a9d25e11f315830dc09150d5762ec9aad9
AidaMillatiMardiana <aidadiana2702@gmail.com>
2022-02-21 20:29:26 +0700

    Praktikum 2 Minggu 2

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        echo "Hi! Selamat Datang";
+        }
+    
+    public function about() {
+            echo "Nama : Aida Millati Mardiana <br>
+            Nim : 2041720071"
+            ;
+        }
+    public function articlepage($id) {
+            return "Halaman Articles dengan ID" . $id;
+            }
+}
